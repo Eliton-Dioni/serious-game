13319254f3e4e118f7372eb50b1cd0728efc2f81
Eliton Dioni Oliveira da Silva <edioni@dimed.com.br>
2020-09-27 16:26:30 -0300

    Initial commit project

---
+++ SCRUM/Assets/Scripts/Player/Moviment.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moviment : MonoBehaviour
+{
+    public float velocity = 5f;
+    public Text texto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        texto.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal * velocity * Time.deltaTime, vertical * velocity * Time.deltaTime, 0));
+
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        interactions(other);
+    }
+
+    void interactions(Collider2D other) {
+        if(other.gameObject.CompareTag("PC") && Input.GetKey(KeyCode.Space)) {
+            texto.enabled = true;
+        }
+    }
+}
